76575cd5d88218faada1508e214d60ddb6f4ea9e
Eliott-rjd <eliott.rajaud@gmail.com>
2020-11-19 12:19:52 +0100

    V2

--- JS/ImageVoyage.js
+++ JS/ImageVoyage.js
@@ -1,8 +1,9 @@
 class ListeVoyage {
     constructor(nom,ville,lien) {
+        this._nom=nom;
         this._ville=ville; 
         this._lien=lien;
-        this._nom=nom;
+        /*this.temperature=temerature (le degre et pas le lien api) */
     }
 }
 
@@ -11,7 +12,7 @@ let Voyage2=new ListeVoyage('Athènes','../Image/Europe/Athènes.jpeg' ,'PageInf
 var Voyage3=new ListeVoyage('Hong-Kong','../Image/Asie/Hong-Kong.jpeg' ,'PageInfosHong-Kong.html');
 var Voyage4=new ListeVoyage('Tokyo','../Image/Asie/Tokyo.jpeg' ,'PageInfosTokyo.html');
 var Voyage5=new ListeVoyage('Sydney','../Image/Océanie/Sydney.jpeg' ,'PageInfosSydney.html');
-var Voyage6=new ListeVoyage('Johannesbyrg','../Image/Afrique/Johannesburg.jpeg' ,'PageInfosJohannesburg.html');
+var Voyage6=new ListeVoyage('Johannesburg','../Image/Afrique/Johannesburg.jpeg' ,'PageInfosJohannesburg.html');
 var Voyage7=new ListeVoyage('Marrakech','../Image/Afrique/Marrakech.jpeg','PageInfosMarrakech.html');
 var Voyage8=new ListeVoyage('Hawaii','../Image/Amérique/Hawaii.jpeg' ,'PageInfosHawaii.html');
 var Voyage9=new ListeVoyage('NewYork','../Image/Amérique/NewYork.jpeg' ,'PageInfosNewYork.html');
@@ -33,6 +34,25 @@ let template = document.querySelector("#ListeVoyage")
     clone.firstElementChild.innerHTML = newContent;
     document.getElementById("Destination").appendChild(clone)
  }
-
-
-
+ /*
+let TVenise = "https://api.openweathermap.org/data/2.5/weather?q=Venise&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TAthènes = "https://api.openweathermap.org/data/2.5/weather?q=Athènes&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let THongKong ="https://api.openweathermap.org/data/2.5/weather?q=Hong Kong&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TTokyo ="https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TSydney ="https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TJohannesburg ="https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TMarrakech ="https://api.openweathermap.org/data/2.5/weather?q=Marrakech&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let THawaii ="https://api.openweathermap.org/data/2.5/weather?q=Hawaii&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TNewYork ="https://api.openweathermap.org/data/2.5/weather?q=New York&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+let TLasVegas ="https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric"
+const ListeTemperature = [TVenise,TAthènes,TTokyo,TJohannesburg,TMarrakech,THawaii,TNewYork,TLasVegas]
+*/
+ function Température() {
+    fetch("Température")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        document.getElementById("myp").innerHTML= myJson["main"]["temp"] + "°C"
+    })
+  }
