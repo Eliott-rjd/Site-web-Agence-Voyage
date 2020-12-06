abfb43714f965ec91b84de6e0ee4d05b215f7912
Eliott-rjd <eliott.rajaud@gmail.com>
2020-12-06 20:32:47 +0100

    Filtre

--- JS/ImageVoyage.js
+++ JS/ImageVoyage.js
@@ -1,24 +1,29 @@
+window.onload = function () {
+    Filtrage()
+}
+
 class ListeVoyage {
-    constructor(nom,ville,id,continent,prix,temperature) {
+    constructor(nom,ville,id,continent,prix,animaux,temperature) {
         this._nom=nom;
         this._ville=ville; 
         this._id=id;
         this._continent=continent;
         this._prix=prix;
+        this._animaux=animaux;
         this._temperature=temperature;
     }
 }
 
-let Voyage1=new ListeVoyage('Venise','../Image/Europe/Venise.jpeg' ,'1','Europe','200',"https://api.openweathermap.org/data/2.5/weather?q=Venise&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-let Voyage2=new ListeVoyage('Athènes','../Image/Europe/Athènes.jpeg' ,'2','Europe','100',"https://api.openweathermap.org/data/2.5/weather?q=Athènes&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage3=new ListeVoyage('Hong-Kong','../Image/Asie/Hong-Kong.jpeg' ,'3','Asie','500',"https://api.openweathermap.org/data/2.5/weather?q=Hong Kong&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage4=new ListeVoyage('Tokyo','../Image/Asie/Tokyo.jpeg' ,'4','Asie','800',"https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage5=new ListeVoyage('Sydney','../Image/Océanie/Sydney.jpeg' ,'5','Océanie','800',"https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage6=new ListeVoyage('Johannesburg','../Image/Afrique/Johannesburg.jpeg' ,'6','Afrique','100',"https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage7=new ListeVoyage('Marrakech','../Image/Afrique/Marrakech.jpeg','7','Afrique','200',"https://api.openweathermap.org/data/2.5/weather?q=Marrakech&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage8=new ListeVoyage('Hawaii','../Image/Amérique/Hawaii.jpeg' ,'8','Amérique','1000',"https://api.openweathermap.org/data/2.5/weather?q=Hawaii&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage9=new ListeVoyage('NewYork','../Image/Amérique/NewYork.jpeg' ,'9','Amérique','1000',"https://api.openweathermap.org/data/2.5/weather?q=New York&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
-var Voyage10=new ListeVoyage('LasVegas','../Image/Amérique/LasVegas.jpeg' ,'10','Amérique','900',"https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+let Voyage1=new ListeVoyage('Venise','../Image/Europe/Venise.jpeg' ,'1','0','200','true',"https://api.openweathermap.org/data/2.5/weather?q=Venise&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+let Voyage2=new ListeVoyage('Athènes','../Image/Europe/Athènes.jpeg' ,'2','0','100','false',"https://api.openweathermap.org/data/2.5/weather?q=Athènes&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage3=new ListeVoyage('Hong-Kong','../Image/Asie/Hong-Kong.jpeg' ,'3','2','500','false',"https://api.openweathermap.org/data/2.5/weather?q=Hong Kong&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage4=new ListeVoyage('Tokyo','../Image/Asie/Tokyo.jpeg' ,'4','2','800','true',"https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage5=new ListeVoyage('Sydney','../Image/Océanie/Sydney.jpeg' ,'5','4','800','true',"https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage6=new ListeVoyage('Johannesburg','../Image/Afrique/Johannesburg.jpeg' ,'6','3','100','true',"https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage7=new ListeVoyage('Marrakech','../Image/Afrique/Marrakech.jpeg','7','3','200','false',"https://api.openweathermap.org/data/2.5/weather?q=Marrakech&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage8=new ListeVoyage('Hawaii','../Image/Amérique/Hawaii.jpeg' ,'8','1','1000','true',"https://api.openweathermap.org/data/2.5/weather?q=Hawaii&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage9=new ListeVoyage('NewYork','../Image/Amérique/NewYork.jpeg' ,'9','1','1000','true',"https://api.openweathermap.org/data/2.5/weather?q=New York&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
+var Voyage10=new ListeVoyage('LasVegas','../Image/Amérique/LasVegas.jpeg' ,'10','1','900','false',"https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=5c94e08c097f3277a355b6688d8a6b24&units=metric");
 
 
 const ListeVoyages = [Voyage1,Voyage2,Voyage3,Voyage4,Voyage5,Voyage6,Voyage7,Voyage8,Voyage9,Voyage10];
@@ -29,16 +34,17 @@ let template = document.querySelector("#ListeVoyage");
 const initial = document.getElementById('Destination').innerHTML;
 
 
+
 function Filtrage() {
     var VoyageFiltre = [];
-    
     div = document.getElementById('Destination');
     div.innerHTML=initial;
 
     prixMax = parseInt(document.getElementById('prixmax').value);
-    console.log(prixMax)
+    FiltreContinent =parseInt(document.getElementById('continents').value);
+    
     for (i = 0; i < ListeVoyages.length; i++) {
-        if (ListeVoyages[i]['_prix'] <= prixMax) {
+        if (ListeVoyages[i]['_prix'] <= prixMax && (ListeVoyages[i]['_continent'] == FiltreContinent ||FiltreContinent == 5)) {
             console.log(ListeVoyages[i]['_prix'])
             VoyageFiltre.push(ListeVoyages[i])
             console.log(VoyageFiltre)
@@ -59,7 +65,6 @@ function Filtrage() {
 }
 
 }
-
 
 
 //function Température() {
--- JS/JavaPourTous.js
+++ JS/JavaPourTous.js
@@ -29,60 +29,15 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-//Pris sur Howto
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
 
-// Afficher les élements filtré
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// Cacher les élements non selec
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-for (var i = 0; i < btnsFiltre.length; i++) {
-  btnsFiltre[i].addEventListener("click", function() {
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
 
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("principal").style.marginLeft= "0";
 
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("principal").style.marginLeft= "0";
 
 }
