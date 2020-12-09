14d91fd658044ca75141fc59dbbe83fd49f65492
Eliott-rjd <eliott.rajaud@gmail.com>
2020-12-09 22:47:36 +0100

    Moncompte

--- JS/MonCompte-info.js
+++ JS/MonCompte-info.js
@@ -1,5 +1,15 @@
+var MonPrenom ;
 
 
+window.onload = function () {
+    mybutton = document.getElementById('myBtn')
+    MonPrenom = document.getElementById('Prenom').value;
+    chgt()
+  }
 
-var email = localStorage.getItem('user')
-document.getElementById('email').setAttribute('value',email) 
+
+var Monemail = localStorage.getItem('user')
+function chgt() {
+document.getElementById('email').innerHTML = Monemail
+document.getElementById('MonPrénom').innerHTML = MonPrenom
+}
